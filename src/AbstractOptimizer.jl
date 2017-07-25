ffc5e866ff81a16b0ed6c3918fd408d41ea45253
Jacob Carey <JCarey@telligen.com>
2017-07-25 13:38:01 -0400

    Cause error if `update` not implemented

--- src/AbstractOptimizer.jl
+++ src/AbstractOptimizer.jl
@@ -2,4 +2,4 @@ abstract type Optimizer
 end
 
 "Calculate change in parameters for gradient descent"
-update(opt::Optimizer, g_t::Array{Float64}) = g_t
+update(opt::Optimizer, g_t::Array{Float64}) = error("not implemented")

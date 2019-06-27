1a7de9ace686a7abbd0175c011b86e46dbe3e46f
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2019-06-27 15:09:21 +0200

    Added doc for momentum

--- src/MomentumOptimizer.jl
+++ src/MomentumOptimizer.jl
@@ -1,3 +1,19 @@
+"""
+**Momentum constructor**
+
+```julia
+    Momentum(; η::Real=0.01, γ::Real=0.9)
+```
+
+Algorithm :
+
+```math
+\\begin{align*}
+v_t =& \\gamma v_{t-1} + \\eta g_t\\\\
+\\Delta x_t =& v_t
+\\end{align*}
+```
+"""
 mutable struct Momentum <: Optimizer
     opt_type::String
     t::Int64
@@ -6,7 +22,8 @@ mutable struct Momentum <: Optimizer
     v_t::AbstractArray
 end
 
-"Construct Momentum optimizer"
+## Note the algorithm seems flawed, \η should be 1-\β and a supplementary global learning rate would be nice
+
 function Momentum(; η::Real=0.01, γ::Real=0.9)
     @assert η > 0.0 "η must be greater than 0"
     @assert γ > 0.0 "γ must be greater than 0"
